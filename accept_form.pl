e1e529529bb30c975d7d044a538d166f18479acf
dcaprani <dcaprani@me.com>
2014-12-10 18:40:57 +0000

    WebContentCommit

--- accept_form.pl
+++ accept_form.pl
@@ -20,4 +20,23 @@ exit;
 sub insertDB {
 my $name = shift;
 my $address =shift;
-:
+
+my $dbhost='127.0.0.1'; my $dbport=3306;
+my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
+$dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
+                ) || die "Could not connect to database: $DBI::errstr";
+$sth = $dbh->prepare("insert into custdetails(name,address) values(?,?)");
+$sth->execute($name,$address);
+}
+
+sub showDB {
+my $dbhost='127.0.0.1'; my $dbport=3306;
+my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
+$dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
+                ) || die "Could not connect to database: $DBI::errstr";
+$sth = $dbh->prepare("select * from custdetails");
+$sth->execute();
+while (my $result = $sth->fetchrow_hashref()) {
+        print $result->{'name'}," ",$result->{'address'},"<p>";
+}
+}
